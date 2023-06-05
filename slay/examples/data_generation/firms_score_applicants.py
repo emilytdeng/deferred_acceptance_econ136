32d83b1a8ebedd1d4e6912210d7d975b063ef857
Emily Deng <emilydeng@DNa1da209.SUNet>
2023-06-05 15:14:28 -0700

    wahhhh

--- slay/examples/data_generation/firms_score_applicants.py
+++ slay/examples/data_generation/firms_score_applicants.py
@@ -11,7 +11,7 @@ def generate_resume_scores(firms, applicants):
     # what we will return: a dictionary representing each firms resume score of all the applicants
     firms_score_applicants = {}
     # the mean score for each applicant, used to create a normal distribution 
-    mean_scores = np.zeros(N)
+    mean_scores = np.zeros(N, dtype=int)
 
     # for each applicant
     for a in range(0, N):
@@ -25,8 +25,7 @@ def generate_resume_scores(firms, applicants):
         actual_scores = []
         # for each applicant, sample a resume score based on their previosly decided mean score
         for a in range(0, N):
-            print(a)
-            sample = norm.rvs(loc=mean_scores[a], size=1)
+            sample = norm.rvs(loc=mean_scores[a], size=1)[0] # first element of resulting array, so 'sample' is an integer
             app_res_score = {'name': applicants[a], 'score': sample}
             # actual_scores is a list of dictionaries where applicants are matched with their resume scores
             actual_scores.append(app_res_score)
@@ -43,28 +42,44 @@ def resume_screening(firms, applicants, firms_score_applicants, keep):
     N = len(applicants) 
     firms_score_applicants_top = {} # firms' applicant scores after they have truncated their list based on resume scores
 
-    k = math.floor(keep * F) # the number of applicants we will interview after resume round
+    k = math.floor(keep * N) # the number of applicants we will interview after resume round
+    print('k',k)
     for f in range(0, F):
-        sorted_res_scores = sorted(firms_score_applicants[f], key=lambda x: x['score'], reverse=True)
-        firms_score_applicants_top[f] = sorted_res_scores[:keep] # truncate to only keep top k candidates
+        firm_name = firms[f]
+        sorted_res_scores = sorted(firms_score_applicants[firm_name], key=lambda x: x['score'], reverse=True)
+        firms_score_applicants_top[firm_name] = sorted_res_scores[:k] # truncate to only keep top k candidates
         
     return firms_score_applicants_top
 
 # converting our dictionaries to a format that is compatible with DA code (previous two functions return dictionary of dictionaries)
-def convert_to_preferences(firms, applicants, firms_score):
+def convert_to_preferences(firms, new_app_list, firms_score_applicants_top):
     F = len(firms)
-    N = len(applicants)
+    N = len(new_app_list)
 
-    # what we will return: a dictionary of firms to their list of applicant preferences where applicants are identified by place in original list
+    # what we will return: a dictionary of firms to their list of applicant preferences where applicants are identified by place in applicant list
     firms_preferences = {}
     # for each firm
     for f in range(0, F):
-        applicant_scores = firms_score[f] # a list of dictionaries where each item is {'name': name, 'score', score}
+        firm_name = firms[f]
+        # a list of dictionaries where each item is {'name': name, 'score', score}
+        applicant_scores = firms_score_applicants_top[firm_name] 
+        # a list of dictionaries but sorted numerically by score highest to lowest
         sorted_applicant_scores = sorted(applicant_scores, key=lambda x: x['score'], reverse=True)
-        preference = np.zeros(N) # the firm's preferences as a list where applicants are numbers based on their place in OG applicants list
+        preference = [] # the firm's preferences as a list where applicants are numbers based on their place in resume screened list
         for a in range(0, N):
-            index = applicants.index(sorted_applicant_scores['name']) + 1 # applicant's original place in 'applicants' list
-            preference[a] = index
-        firms_preferences[f] = preference 
+            applicant_name = sorted_applicant_scores[a]['name']
+            index = new_app_list.index(applicant_name) + 1 # applicant's place in resume screened list
+            preference.append(index)
+        firms_preferences[firm_name] = preference 
 
     return firms_preferences
+
+# converts a list of format [{'name': name, 'score', score}, {'name': name, 'score', score}, ...] to a list of applicants
+# used to build new applicant_list for each firm after initial resume screening
+def dict_to_list(firms_score_resume_top):
+    N = len(firms_score_resume_top) # number of remaining applicants after resume screening
+    new_app_list = []
+    for a in range(0, N):
+        applicant_name = firms_score_resume_top[a]['name']
+        new_app_list.append(applicant_name)
+    return new_app_list
--- slay/examples/simple_job_match.py
+++ slay/examples/simple_job_match.py
@@ -1,7 +1,7 @@
 from deferred_acceptance.deferred_acceptance import deferred_acceptance
 from deferred_acceptance.utils import create_dataframes
 from data_generation.applicants_rank_firms import applicants_rank_firms
-from data_generation.firms_score_applicants import generate_resume_scores, resume_screening, convert_to_preferences
+from data_generation.firms_score_applicants import generate_resume_scores, resume_screening, convert_to_preferences, dict_to_list
 
 
 def simple_job_match() -> None:
@@ -9,28 +9,32 @@ def simple_job_match() -> None:
     Here is a minimalistic example of deferred acceptance algorithm for firm-applicant job matching.
     """
     # Prepare the dataframes
-    applicants = ["emily", "shaurya", "melina", "ellie"]
+    applicants = ["emily", "shaurya", "melina", "ellie", "maya", "kyle", "ethan", "jamie", "ari", "chelsea"]
     firms = ["McKinsey", "Bain", "BCG"]
     students_preferences = applicants_rank_firms(applicants, firms, 1)
     res_scores = generate_resume_scores(firms, applicants)
-    print(res_scores)
-    res_screen = resume_screening(firms, applicants, res_scores, .2)
-    print(res_scores)
-    schools_preferences = convert_to_preferences(firms, applicants, res_screen)
-    print(schools_preferences)
+    #print(res_scores)
+    # each firm conducts resume screening and only picks their top 40% of candidates to interview
+    for firm in firms:
+        res_screen = resume_screening(firms, applicants, res_scores, 1)
+        #print('res_screen', res_screen)
+        screened_applicants = dict_to_list(res_screen)
+        print(screened_applicants)
+        schools_preferences = convert_to_preferences(firms, screened_applicants, res_screen)
+        #print(schools_preferences)
 
-    students_df, schools_df = create_dataframes(
-        students_list=applicants,
-        students_preferences=students_preferences,
-        schools_list=firms,
-        schools_preferences=schools_preferences,
-    )
+        students_df, schools_df = create_dataframes(
+            students_list=screened_applicants,
+            students_preferences=students_preferences,
+            schools_list=firms,
+            schools_preferences=schools_preferences,
+        )
 
-    # Run the algorithm
-    schools_quota = {"McKinsey": 1, "Bain": 2, "BCG": 1}
-    matches = deferred_acceptance(
-        students_df=students_df, schools_df=schools_df, schools_quota=schools_quota
-    )
+        # Run the algorithm
+        schools_quota = {"McKinsey": 1, "Bain": 2, "BCG": 1}
+        matches = deferred_acceptance(
+            students_df=students_df, schools_df=schools_df, schools_quota=schools_quota
+        )
 
     print(matches)
 
